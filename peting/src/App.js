e6f9e0ae29ffe264be58367c68790e9536ae7aa8
Brendan Young <young.brendan6@gmail.com>
2022-08-18 17:19:28 +1000

    create-react-app installed

---
+++ peting/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom'
+
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a test</h1>
+
+     
+      < Form />
+
+
+    </div>
+  );
+}
+
+export default App;
